71c2d8a60990fe3c1c40592c40630924198eb539
Vitor Suzuki <vitor.szk@outlook.com>
2021-05-26 15:41:13 -0300

    Correção dos inputs do filtro por nome

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -4,10 +4,8 @@ import axios from 'axios';
 export default class Filters extends Component {
 
     render() {
-        const listFilter = this.listFilter()
         return (
             <div>
-                listFilter={listFilter}
                 <input type="text" placeholder="O que você precisa?" value={this.props.nameFilter} onChange={this.props.handleChangeName}></input>
                 <button onClick={() => this.handleChangeName()}>Buscar</button>
             </div>
