94ed7b41f3ee98539af7355d9d83d38a196f73fc
alvesgarrido <matheus.agarrido@gmail.com>
2021-05-26 14:41:52 -0300

    Implementação da funcionalidade do cadastro de serviços

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 
+const baseUrl = "https://labeninjas.herokuapp.com"
+const demoKey = "e2190c39-7930-4db4-870b-bed0e5e4b88e"
+
 export class AppContainer extends Component {
+
   render() {
     return (
       <div>
