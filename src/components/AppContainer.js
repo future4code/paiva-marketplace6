af6624dbf8fb438f2291bc1350fbf5310b325962
Vitor Suzuki <vitor.szk@outlook.com>
2021-05-26 14:51:48 -0300

    Filtro por nome iniciado

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
+import Filters from './Filters'
 
 export class AppContainer extends Component {
+
+  state = {
+    minFilter: 0,
+    maxFilter: 10000,
+    nameFilter: ""
+  }
+
+  handleChangeName = (e) => {
+    this.setState({ nameFilter: e.target.value })
+  }
+
   render() {
     return (
       <div>
-        <p>Pronto para começar!</p>
+        <Filters
+          nameFilter={this.state.nameFilter}
+          handleChangeName={this.handleChangeName}
+        />
       </div>
     )
   }
